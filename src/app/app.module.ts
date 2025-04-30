eb3c9c01323a6a2380a7ac15d052fc7ae23b0a88
EdmundoGarcia <edmundo.garcia4191@alumnos.udg.mx>
2025-04-30 11:29:07 -0600

    Created components "task" and "timer"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
